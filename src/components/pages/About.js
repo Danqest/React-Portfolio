f1b94b21a901d2ac930a20e069eb6523ee45affd
Colin Brooks <mcbrooks@pm.me>
2022-08-11 16:47:25 -0400

    edited some working components

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React from "react";
+import bateman from "../../images/bateman-img.jpg";
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div className="container" style={{ "margin-top": "15px" }}>
+      <div className="card mb-3 bg-secondary" style={{ "max-width": "1200px" }}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src={bateman}
+              className="img-fluid rounded-start"
+              alt="avatar photo of Patrick Bateman smiling"
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h1 className="card-title">About Me</h1>
+              <p className="card-text">
+                My name is Colin Brooks. I am a self-taught Python programmer
+                with an academic background in finance & investing. I have
+                worked in commercial banking as a credit analyst and underwriter
+                for the past four years and have a great interest in the
+                investment industry. In the pursuit of career development, I
+                have been looking to break into the tech industry which has
+                spurred my enrollment into the Georgia Tech Coding Bootcamp in
+                order to learn full-stack development.
+              </p>
+              <p className="card-text">
+                Over the past couple of years I have been teaching myself about
+                artificial intelligence with deep learning & neural networks.
+                Lately my personal side-projects have included the exploration
+                of unsupervised learning for the purposes of stock trading using
+                a customizes OpenAI Gym environment. I also am a co-founder and
+                owner of a cryptocurrency mining company located in Melbourne,
+                FL.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,156 @@
+import React from "react";
+import testChart from "../../images/testchart.png";
+import weatherDashboard from "../../images/weather-dashboard.png";
+import passwordGenerator from "../../images/password-generator.png";
+import screenshotTeam from "../../images/screenshot-team.png";
+import stockAlertTracker from "../../images/stock-alert-tracker.png";
+
+
+
+
+export default function Portfolio() {
+  return (
+    <center>
+      <h1>Portfolio Page</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="card bg-secondary" style={{ width: "30rem" }}>
+              <h2 className="card-text">Crypto Sentiment</h2>
+              <img
+                src={testChart}
+                className="card-img-top"
+                alt="screenshot of bitcoin price & sentiment"
+              />
+              <div className="card-body">
+                <a
+                  href="https://github.com/Danqest/07-Crypto-Sentiment-Index"
+                  target="_blank"
+                  className="btn btn-primary m-1"
+                >
+                  Github Repo
+                </a>
+                <a
+                  href="https://danqest.github.io/07-Crypto-Sentiment-Index/"
+                  target="_blank"
+                  className="btn btn-primary m-1"
+                >
+                  Deployed Application
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="row" style={{ "margin-top": "20px" }}>
+            <div className="col">
+              <div className="card bg-secondary" style={{ width: "30rem" }}>
+                <h2 className="card-text">Weather Dashboard</h2>
+                <img
+                  src={weatherDashboard}
+                  className="card-img-top"
+                  alt="screenshot of the weather dashboard application"
+                />
+                <div className="card-body">
+                  <a
+                    href="https://github.com/Danqest/06-Server-Side-APIs-Weather-Dashboard"
+                    target="_blank"
+                    className="btn btn-primary m-1"
+                  >
+                    Github Repo
+                  </a>
+                  <a
+                    href="https://danqest.github.io/06-Server-Side-APIs-Weather-Dashboard/"
+                    target="_blank"
+                    className="btn btn-primary m-1" 
+                  >
+                    Deployed Application
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card bg-secondary" style={{ width: "30rem" }}>
+                <h2 className="card-text">Password Generator</h2>
+                <img
+                  src={passwordGenerator}
+                  className="card-img-top"
+                  alt="screenshot of the password generator application"
+                />
+                <div className="card-body">
+                  <a
+                    href="https://github.com/Danqest/03-Javascript-Password-Generator"
+                    target="_blank"
+                    className="btn btn-primary m-1"
+                  >
+                    Github Repo
+                  </a>
+                  <a
+                    href="https://danqest.github.io/03-Javascript-Password-Generator/"
+                    target="_blank"
+                    className="btn btn-primary m-1"
+                  >
+                    Deployed Application
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row" style={{ "margin-top": "20px" }}>
+            <div className="col">
+              <div className="card bg-secondary" style={{ width: "30rem" }}>
+                <h2 className="card-text">Team Profile Generator</h2>
+                <img
+                  src={screenshotTeam}
+                  className="card-img-top"
+                  alt="screenshot of the team builder application"
+                />
+                <div className="card-body">
+                  <a
+                    href="https://github.com/Danqest/10-OOP-Team-Profile-Generator"
+                    target="_blank"
+                    className="btn btn-primary m-1"
+                  >
+                    Github Repo
+                  </a>
+                  <a
+                    href="https://drive.google.com/file/d/1PCgl4r6LFv064hIkwemHQJGAZWmripmO/view"
+                    target="_blank"
+                    className="btn btn-primary m-1"
+                  >
+                    Walkthru Video
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card bg-secondary" style={{ width: "30rem" }}>
+                <h2 className="card-text">Stock Alert Tracker</h2>
+                <img
+                  src={stockAlertTracker}
+                  className="card-img-top"
+                  alt="screenshot of the stock alert tracker application"
+                />
+                <div className="card-body">
+                  <a
+                    href="https://github.com/Danqest/15-Project2-Stock-Alert-Tracker"
+                    target="_blank"
+                    className="btn btn-primary m-1"
+                  >
+                    Github Repo
+                  </a>
+                  <a
+                    href="https://lit-waters-98495.herokuapp.com/"
+                    target="_blank"
+                    className="btn btn-primary m-1"
+                  >
+                    Deployed Application
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </center>
+  );
+}
