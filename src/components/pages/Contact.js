874486aa79fff45a474177283a376744d5aa3bf1
Colin Brooks <mcbrooks@pm.me>
2022-08-11 18:27:47 -0400

    functioning contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,59 @@
-import React from 'react';
+import React from "react";
+
+function multiFunctionHandler(e) {
+  checkIfFilled(e)
+  emailIsValid(e)
+}
+
+function checkIfFilled(e) {
+  // console.log("fugg")
+  const value = e.target.value;
+  if (value == '') {
+    window.alert('Please make sure to fill in all fields.')
+  }
+}
+
+function emailIsValid(e) {
+  const value = e.target.value
+  if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    window.alert('Please enter a valid email address format.')
+  }
+}
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-    </div>
+    <center>
+      <div className="container">
+        <h1>Contact Page</h1>
+        <h2></h2>
+        <form>
+          <label>Your Name:</label>
+          <div className="column">
+            <input
+              type="textarea"
+              name="name"
+              onMouseLeave={checkIfFilled}
+            ></input>
+          </div>
+          <label>Your Email Address:</label>
+          <div className="column">
+            <input
+              type="textarea"
+              name="email"
+              onMouseLeave={multiFunctionHandler}
+            ></input>
+          </div>
+          <label>Message:</label>
+          <div className="column">
+            <textarea
+              type="textarea"
+              name="textarea"
+              onMouseLeave={checkIfFilled}
+            ></textarea>
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </center>
   );
 }
